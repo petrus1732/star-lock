36fc69007225b5c001d098b16f0218120f52ecc7
Peter <peter920508@gmail.com>
2023-09-01 17:45:09 +0800

    animation

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const R = 120;
-let phi = 2.3;
+let phi = 5.55;
 const H = 533.6;
 const spots = [];
+const degree = [0, 0, 0, 0, 0, 0];
+const spin = [0, 0, 0, 0, 0, 0];
+const linkHalf = 1.5; //the length of the half of the linking line
 const key = 'CYCLE';
-let password;
+let password = 'brglegusno';
+
+const center = {x:0, y:0}
+
+const script = '恭喜你解開了全部的謎題，你說寶藏在哪裡？先別這麼著急，這趟探險好玩嗎？想想當初自己開始探險的初衷吧，可能是因為那價值連城的寶藏讓你心動，又或是你想要四處遊歷，看看世界。無論如何，還記得第一次探險時的心情嗎？成功解密的喜悅，危機進逼的緊張，或是期望落空的失望。這些都是每一趟探險無價的寶藏，不是嗎？希望你們能夠再次想起第一次探險的種種感受。'
 
 let clicks = 0;
 let stage = 0;
@@ -11,8 +18,10 @@ let passwordPos = 0;
 let letterPos = 0;
 let start;
 
+//document.getElementById('first').style.display = 'none';
+//stage++;
+
 const setUp = () => {
-  console.log(start);
   switch (start) {
     case 'eg': 
       phi += 2*Math.PI/5;
@@ -53,7 +62,6 @@ document.querySelector("form").addEventListener(
   false,
 );
 
-
 const addAnimation = (i) => {
   document.getElementById(`s${i}`).style.animation = `1s infinite alternate blink`;
 }
@@ -63,6 +71,9 @@ const onClick = (e) => {
     e.style.backgroundColor = 'red'
     e.style.filter = 'blur(3px)'
     e.style.border = 'none';
+    if (clicks >= 1) {
+      link(clicks);
+    }
     clicks += 1;
     for (let i = clicks; i > 0; i--) {
       document.getElementById(`s${i}`).style.animation = 'none';
@@ -71,8 +82,14 @@ const onClick = (e) => {
     if (clicks === 5) {
       stage++; 
       document.getElementById('key').style.display = 'flex';
+      setTimeout(() => link(5), 1000);
     }
   }
+}
+
+const link = (n) => {
+  const line =  document.getElementById(`l${n}`);
+  line.style.animation = '0.5s linear draw-line forwards';
 }
 
 const setLengths = () => {
@@ -80,10 +97,8 @@ const setLengths = () => {
     x: window.innerWidth/2,
     y: window.innerHeight/2
   }
-  const center = {
-    x: mid.x - 60,
-    y: mid.y - 35 ,
-  }
+  center.x = mid.x - 60;
+  center.y = mid.y - 35;
 
   // setting of guide sentence and the key
   document.getElementById('guide').style.marginTop = `${(mid.y - H/2) / 2}px`;
@@ -93,12 +108,28 @@ const setLengths = () => {
   const map = document.getElementById('map');
   map.style.left = `${mid.x}px`;
   map.style.top = `${mid.y}px`;;
-  
-  // correct spots
+
+  // correct spots and linking line
   for (let i = 1; i <= 5; i++) {
-    const spot = document.getElementById(`s${i}`)
-    spot.style.left = `${center.x + R*Math.sin(-4*Math.PI/5 * i + phi)}px`;
-    spot.style.top = `${center.y + R*Math.cos(-4*Math.PI/5 * i + phi)}px`;
+    const spot = document.getElementById(`s${i}`);
+    const line = document.getElementById(`l${i}`);
+    spot.style.left = `${center.x + R*Math.cos(4*Math.PI/5 * i + phi)}px`;
+    spot.style.top = `${center.y + R*Math.sin(4*Math.PI/5 * i + phi)}px`;
+
+    line.style.transformOrigin = `${-R*Math.cos(162*Math.PI/180)}px ${R*Math.sin(162*Math.PI/180)}px`;
+    line.style.left = `${center.x + R*Math.cos(162*Math.PI/180)}px`;
+    line.style.top = `${center.y - R*Math.sin(162*Math.PI/180)}px`;
+
+    degree[i] = 144*i + phi * 180 / Math.PI + 162;
+    const start = `scale(1) rotate(${degree[i]}deg) translateY(${-linkHalf}px`;
+    const end = `scale(30) rotate(${degree[i]+360*10}deg) translateY(${-linkHalf}px`;
+    line.style.transform = start;
+    document.documentElement.style.setProperty(`--sps${i}`, start);
+    document.documentElement.style.setProperty(`--spe${i}`, end);
+    /*spin[i] = [
+      [{ transform: start }, { transform: end }],
+      [{ duration: 3000, iterations: 1, fill:'both'}]  
+    ];*/
   }
   
   //other spots
@@ -128,7 +159,28 @@ const setLengths = () => {
   circle.style.top = `${center.y}px`; */
 };
 
-const theEnd = () => {}
+const createWords = (chr) => {
+  const endContainer = document.getElementById('end');
+  const chrContainer = document.createElement('div');
+  chrContainer.setAttribute('class', 'end-letter');
+  chrContainer.innerHTML = chr;
+  endContainer.appendChild(chrContainer);
+}
+
+
+const theEnd = () => {
+  setTimeout(() => document.getElementById('end-container').style.display = 'flex', 3000);
+  let time = 3500
+  for (let i = 0; i < script.length; i++) {
+    time += 80;
+    if (i > 0 && (script[i-1] === '，' || script[i-1] === '？' || script[i-1] === '。')) time += 250;
+    setTimeout(() => createWords(script[i]),  time);
+  }
+
+  setTimeout(() => document.getElementById('tag').style.animation = `show 1s forwards`, time + 100);
+  setTimeout(() => document.getElementById('pass').style.display = `block`, time + 1100);
+    
+}
 
 addEventListener("resize", () => setLengths());
 addEventListener("keydown", e => {
@@ -152,6 +204,18 @@ addEventListener("keydown", e => {
         for (let i = 1; i <= 11; i++) {
           document.getElementById(`s${i}`).style.animation = 'show 1s reverse both';
         }
+        for (let i = 1; i <= 5; i++) {
+          //console.log(document.getElementById(`l${i}`), spin);
+          //document.getElementById(`l${i}`).animate(spin[i][0], spin[i][1])
+          const line = document.getElementById(`l${i}`)
+          line.style.width = '228.2536px';
+          line.style.animation = `spin${i} 3s ease-in forwards`;
+        }
+        const white = document.getElementById('white');
+        white.style.left = center.x + 'px';
+        white.style.top = center.y + 'px';
+        white.style.animation = 'magnify 3s ease-in forwards'
+        theEnd();
       }
     }
   }
